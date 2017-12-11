2bbca7e1f3854d70352989f8321bd6b622d44159
Eric Friday <ericfriday@gmail.com>
2017-12-10 19:26:58 -0600

    Create pe-2.cs

---
+++ .gitignore/pe-2.cs
@@ -0,0 +1,25 @@
+using System;
+					
+public class Program
+{
+	public static int Main()
+	{
+	int a = 1;
+	int b = 1;
+	int c = 0;
+	int evens = 0;
+	
+	for (int i = 1; c < 4000000; i++)
+	{
+		if (c % 2 == 0) 
+		{
+			evens = evens + c;
+		}
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	Console.WriteLine(evens);
+	return evens;
+	}
+}
