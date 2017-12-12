2fb3685cf997a89401c5551f6332bcebb4c42597
Eric Friday <ericfriday@gmail.com>
2017-12-12 15:34:32 -0600

    Update pe-1.cs

--- pe-1.cs
+++ pe-1.cs
@@ -5,7 +5,7 @@ public class Program
 	public static int Main()
 	{
 		int count = 0;
-		for (int i = 0; i <= 1000; i++)
+		for (int i = 0; i < 1000; i++)
 		{
 			if (i % 3 == 0 || i % 5 == 0)
 			{
